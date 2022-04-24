3145b8409ba59b2e49c11b748807247d20b895c2
Cham An <tramanh18111998@gmail.com>
2022-04-24 13:14:11 +0300

    QP solve (8 forces)

--- Brick/main.py
+++ Brick/main.py
@@ -8,76 +8,7 @@ import matplotlib.animation as animation
 from matrices import *
 from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 from scipy.spatial.transform import Rotation
-
-
-def cross_product(p, q):
-    return [p[1]*q[2] - p[2]*q[1],
-            p[2]*q[0] - p[0]*q[2],
-            p[0]*q[1] - p[1]*q[0]]
-
-
-def quaternion_product(p,q):
-    Q = np.array([[p[0], -p[1], -p[2], -p[3]],
-                  [p[1],  p[0], -p[3],  p[2]],
-                  [p[2],  p[3],  p[0], -p[1]],
-                  [p[3], -p[2],  p[1],  p[0]]])
-    return Q @ q
-
-
-def conjugate(q):
-    return np.array([q[0], -q[1], -q[2], -q[3]])
-
-
-def skew(v):
-    return np.array([[0, -v[2], v[1]],
-                     [v[2], 0, -v[0]],
-                     [-v[1], v[0], 0]])
-
-
-def D():
-    m, I = system_params['m'], system_params['I']
-    ms = m * np.eye(3)
-    Is = I * np.eye(3)
-    zeros = np.zeros((3,3))
-
-    D = np.block([[ms, zeros],
-                  [zeros, Is]])
-    return D
-
-
-def h():
-    m, g = system_params['m'], system_params['g']
-    h = np.vstack([np.array([[0], [m*g]]), np.zeros((4,1))])
-    return h
-
-
-def B():
-    state = [0.5,0.5,0.5,  # pos
-             0.1,0.2,0.14,  # orient
-             0,0.3,0.4,0.2]  # quaternion
-    rc = state[:3]
-    wc = state[3:6]
-    l = system_params['length']
-    w = system_params['width']
-
-    mat_rc_wc = Tx(rc[0]) @ Ty(rc[1]) @ Tz(rc[2]) @ Rx(wc[0]) @ Ry(wc[1]) @ Rz(wc[2])
-
-    c1 = (mat_rc_wc @ Tx(-l/2) @ Ty(w/2))[0:3, 3]
-    c2 = (mat_rc_wc @ Tx( l/2) @ Ty(w/2))[0:3, 3]
-    c3 = (mat_rc_wc @ Tx( l/2) @ Ty(-w/2))[0:3, 3]
-    c4 = (mat_rc_wc @ Tx(-l/2) @ Ty(-w/2))[0:3, 3]
-
-    s1 = c1 - rc
-    s2 = c2 - rc
-    s3 = c3 - rc
-    s4 = c4 - rc
-
-    eye3 = np.eye(3)
-    B1 = np.tile(eye3, (1,4))
-    B2 = np.hstack([skew(s1), skew(s2), skew(s3), skew(s4)])
-    B = np.vstack([B1, B2])
-    return B
-# ----------------------------------------------------------------------------------------------------------------------------------
+from numpy import array, sin, cos, pi, zeros, cross
 
 def desired_trajectory(t, traj_params, period):
     # desired position, velocity, quaternion, angular velocity
@@ -88,51 +19,17 @@ def desired_trajectory(t, traj_params, period):
     return p_d, v_d, q_d, angVel_d
 
 
-def vect2quat(v):
-    return np.concatenate([[0], v])
-
-
-def quat2vect(q):
-    return q[1:]
-
-
-def ln_quat(q):
-    mod_vectq = np.linalg.norm(q[1:])
-    if mod_vectq < 1e-15:
-        return np.zeros(3)
-    else:
-        return np.array(q[1:])*np.arccos(q[0])*mod_vectq**(-1)
-
-
-def quat2euler_angles(q):
-    phi = np.arctan2((2 * (q[0] * q[1] + q[2] * q[3])), (1 - 2 * (q[1] ** 2 + q[2] ** 2)))
-    theta = np.arcsin(2 * (q[0] * q[2] - q[3] * q[1]))
-    psi = np.arctan2((2 * (q[0] * q[3] + q[1] * q[2])), (1 - 2 * (q[2] ** 2 + q[3] ** 2)))
-    return np.array([phi, theta, psi])
-
-
-def euler2quat(roll, pitch, yaw):
-        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
-        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
-        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
-        return [qw, qx, qy, qz]
-
-
-def system(state, t, u):
+def system(state, t, Q):
     m, g = system_params['m'], system_params['g']
-    # v, dv - velocity and acceleration
-    # q - quaternion
-    # w - angular velocity
 
-    r, v, q, w = state[:3], state[3:6], state[6:10], state[10:13]
+    rc, v, q, w = state[:3], state[3:6], state[6:10], state[10:13]    
 
-    # equation 1 dp=dp
+    # equation 1
     dr = v
 
     # equation 2
-    u_v = u[0]
-    dv = u_v - np.array([0, 0, -g])
+    Q_v = Q[0]
+    dv = m ** (-1) * Q_v - np.array([0, 0, -g])
 
     # equation 3
     dq = -1/2 * quaternion_product(vect2quat(w), q)
@@ -140,9 +37,10 @@ def system(state, t, u):
     # equation 4
     Im = np.array(system_params['I'])
     Im_inv = np.linalg.inv(Im)
-    tau = u[1]
-    T = tau - cross_product(w, Im @ w)
-    dw = np.dot(Im_inv, T)
+
+    tau = Q[1]
+    tau_prime = tau - cross_product(w, Im @ w)
+    dw = Im_inv @ tau_prime
 
     dstate = np.concatenate([dr, dv, dq, dw])
     return dstate
@@ -166,27 +64,140 @@ def system(state, t, u):
 #     return u_pos, u_att
 
 
-def control(state, t, r_d, v_d, q_d, w_d):
-    m = system_params['m']
-
-    K_p = control_params['K1']
-    K_d = control_params['K2']
+def control(state, t, r_d, v_d, q_d, w_d, params):
+    Kp_w = control_params['K1']
+    Kd_w = control_params['K2']
+    
+    Kp_v = control_params['K3']
+    Kd_v = control_params['K4']
+    
+    u_min = control_params['u_min']
+    u_max = control_params['u_max']
 
     r_actual, v_actual, q_actual, w_actual = state[:3], state[3:6], state[6:10], state[10:13]
+    # dr, dv = r_d - r_actual, v_d - v_actual
+
+    param_vector = p(params)
+    # q_star = dv + K_p @ dr
     
-    params = p(system_params)
-
     q_err = quaternion_product(q_d, conjugate(q_actual))
-    if q_err[0]<0:
-        q_err_axis = -np.array(q_err[1:])
-    else:
+    if q_err[0] < 0:
         q_err_axis = np.array(q_err[1:])
+    else:
+        q_err_axis = -np.array(q_err[1:])
 
-    u_w = -K_p @ q_err_axis + K_d @ (w_d - w_actual)
-    u_v = K_p @ (r_d - r_actual) + K_d @ (v_d - v_actual)
-    Y = regressor(w_actual, u_w, u_v)
-    u = Y @ params
-    return m ** (-1) * u[:3], u[3:]
+    a_w = Kp_w @ q_err_axis + Kd_w @ (w_d - w_actual)
+    a_v = Kp_v @ (r_d - r_actual) + Kd_v @ (v_d - v_actual)
+    Y = regressor(w_actual, a_w, a_v)
+
+    Q, F = qp_solve(Y, state, param_vector, u_min, u_max)
+    return Q[:3], Q[3:], F
+
+    # Q = Y @ params
+    # return Q[:3], Q[3:]
+
+
+def qp_solve(Y, state, param_vector, u_min, u_max):
+    Bm = B(state)
+    Vm = V()
+    alpha = 0.0001
+    P = Bm.T @ Bm + alpha * np.eye(8)
+    q = (-1) * param_vector.T @ Y.T @ Bm
+
+    I8 = np.eye(8)
+    G = np.vstack([I8, -I8])
+    h = np.hstack([np.tile([u_max], 8), np.tile([-u_min], 8)])
+
+    A = None
+    b = None
+
+    F = solve_qp(P, q, G, h, A, b)
+    # print(F)
+    # F = np.linalg.pinv(Bm) @ Y @ param_vector
+    Qm = Bm @ F
+    return Qm, F
+
+# G = np.zeros((8,12))
+# G[0, 2] = G[1, 5] = G[2, 8] = G[3, 11] = 1
+# G[4, 2] = G[5, 5] = G[6, 8] = G[7, 11] = -1
+
+# A = np.zeros((8, 12))
+# A[:2, :2] = A[2:4, 3:5] = A[4:6, 6:8] = A[6:, 9:11] = np.eye(2)
+    
+# print(A)
+def B(state):
+
+    l = system_params['length']
+    w = system_params['width']
+
+    rc = state[:3]
+    q  = state[6:10]
+
+    # p1, p2, p3, p4 = get_points(rc, q, l, w)
+
+    # s1 = p1 - rc
+    # s2 = p2 - rc
+    # s3 = p3 - rc
+    # s4 = p4 - rc
+
+    R_trans = quaternion_rotation_matrix(q).transpose()
+    
+    # B1 = np.hstack([R_trans, R_trans, R_trans, R_trans]) #np.tile(R_trans, (1,4))
+    # B2 = (-1) * np.hstack([skew(s1), skew(s2), skew(s3), skew(s4)])
+    # B = np.vstack([B1, B2])
+    positions_vectored = array([[0.156, 0.111, 0.085],
+                                [0.156, -0.111, 0.085],
+                                [-0.156, 0.111, 0.085],
+                                [-0.156, -0.111, 0.085],
+                                [0.0, 0.111, 0.],
+                                [0.0, -0.111, 0.]]).T
+
+
+    positions_heavy = array([[0.156, 0.111, 0.085],
+                            [0.156, -0.111, 0.085],
+                            [-0.156, 0.111, 0.085],
+                            [-0.156, -0.111, 0.085],
+                            [0.120, 0.218, 0.],
+                            [0.120, -0.218, 0.],
+                            [-0.120, 0.218, 0.],
+                            [-0.120, -0.218, 0.]]).T
+
+    # positions of thrusters
+    alphas = array([pi/4, -pi/4,  -pi/4, pi/4])
+    directions = array([1, 1, -1, -1, -1, 1, 1, -1])
+
+    # matrix_vectored
+    matrix_vectored = zeros((6, 6))
+    matrix_heavy = zeros((6, 8))
+    normal_vectors = zeros((3, 8))
+
+    for thruster in range(8):
+        if thruster < 4:
+            normal_vectors[:, thruster] = directions[thruster] * array([cos(alphas[thruster]),
+                                                                    -sin(alphas[thruster]),
+                                                                    0])
+        else:
+            normal_vectors[:, thruster] = directions[thruster] * array([0, 0, 1])
+
+        matrix_heavy[:3, thruster] = R_trans @ normal_vectors[:, thruster]
+        matrix_heavy[3:, thruster] = cross(positions_heavy[:, thruster],
+                                        normal_vectors[:, thruster])
+
+        if thruster < 6:
+            matrix_vectored[:3, thruster] = R_trans @ normal_vectors[:, thruster]
+            matrix_vectored[3:, thruster] = cross(positions_vectored[:, thruster],
+                                                normal_vectors[:, thruster])
+
+    return matrix_heavy
+
+
+def V():
+    V = np.zeros((12,4))
+    V[2, 0] = 1
+    V[5, 1] = 1
+    V[8, 2] = 1
+    V[11, 3] = 1
+    return V
 
 
 def L(w):
@@ -206,53 +217,92 @@ def regressor(w, dw, dv):
     return Y
 
 
-def p(sys_params):
-    m = sys_params['m']
-    Ixx = sys_params['Ixx']
-    Iyy = sys_params['Iyy']
-    Izz = sys_params['Izz']
-    Ixy = sys_params['Ixy']
-    Ixz = sys_params['Ixz']
-    Iyz = sys_params['Iyz']
-    return np.array([m, Ixx, Iyy, Izz, Ixy, Ixz, Iyz])
+def p(params):
+    m = params['m']
+    I = params['I']
+    
+    return np.array([m, I[0][0], I[1][1], I[2][2], I[0][1], I[0][2], I[1][2]])
+
+
+def get_points(rc, q, length, width):
+    dx = length / 2
+    dy = width / 2
+
+    Hc = quaternion_affine_matrix(q, rc)
+
+    H = Hc @ Tx(-dx) @ Ty(dy)
+    p1 = H[:3, 3]
+
+    H = Hc @ Tx(dx) @ Ty(dy)
+    p2 = H[:3, 3]
+
+    H = Hc @ Tx(dx) @ Ty(-dy)
+    p3 = H[:3, 3]
+
+    H = Hc @ Tx(-dx) @ Ty(-dy)
+    p4 = H[:3, 3]
+    return p1, p2, p3, p4
 
 
 def simulate_system(state_init, frequency, t_0, t_final):
     dT = 1/frequency
     t = np.arange(t_0, t_final, dT)
-    t_star = np.linspace(0, dT, 10)
+    t_star = np.linspace(0, dT, 5)
     per = 3
-    state_prev = state_init
-    states, quats, angles = [], [], []
-    U_pos, U_att = [], []
+    state_prev = state_prev_modified = state_init
+    states, states_mod, quats_d, angles = [], [], [], []
+    poss_d = []
+    U_pos, U_att, U_pos_mod = [], [], []
+    Fs = []
     for i in range(len(t)):
         t_curr = t[i]
         pos_d, vel_d, quat_d, angVel_d = desired_trajectory(t_curr, trajectory_params, per)
-        u = control(state_prev, t_curr, pos_d, vel_d, quat_d, angVel_d)
 
-        state = odeint(system, state_prev, t_star,
-                           args=(u,))
+        u_real = control(state_prev, t_curr, pos_d, vel_d, quat_d, angVel_d, system_params)
+        u_modified = control(state_prev_modified, t_curr, pos_d, vel_d, quat_d, angVel_d, control_params)
+
+        state = odeint(system, state_prev, t_star, args=(u_real, ))
+        state_modified = odeint(system, state_prev_modified, t_star, args=(u_modified, ))
+
         state_prev = state[-1]
+        state_prev_modified = state_modified[-1]
 
-        quats.append(state_prev[6:10])
-        # d_w.append(state_prev[4:7])
+        quats_d.append(quat_d)
+        poss_d.append(pos_d)
+
         states.append(state_prev)
-        angle = Rotation.from_quat(state_prev[6:10])
-        angles.append(angle.as_euler('xyz'))
-        U_pos.append(u[0])
-        U_att.append(u[1])
+        states_mod.append(state_prev_modified)
 
+        quat_cur = state_prev[6:10]
+        quat_ = np.array([quat_cur[3], quat_cur[0], quat_cur[1], quat_cur[2]])
+        angle = Rotation.from_quat(quat_)
+        angles.append(angle.as_euler('xyz'))
+
+        U_pos.append(u_real[0])
+        U_pos_mod.append(u_modified[0])
+        U_att.append(u_real[1])
+        Fs.append(u_real[2])  
+
+    quats_d = np.array(quats_d)
+    poss_d = np.array(poss_d)
     states = np.array(states)
-    quats = np.array(quats)
+    states_mod = np.array(states_mod)
     angles = np.array(angles)
     U_pos = np.array(U_pos)
+    U_pos_mod = np.array(U_pos_mod)
     U_att = np.array(U_att)
-
+    Fs = np.array(Fs)
+    print(U_pos[-1])
+    print(U_pos_mod[-1])
 
     figure()
     text = ['$q_0$', '$q_1$', '$q_2$', '$q_3$']
+    colors = ['r', 'g', 'b', 'k']
+    styles = [':', '--', '-', '-.']
     for i in range(4):
-        plot(t, quats[:,i], linewidth=2.0, label=str(text[i]))
+        plot(t, states[:,i+6], color=str(colors[i]), linewidth=2.0, linestyle=str(styles[i]), label=str(text[i]))
+        plot(t, states_mod[:,i+6], linewidth=2.0, linestyle=str(styles[i]), label=str(text[i]))
+        plot(t, quats_d[:, i], color=str(colors[i]), linewidth=1.0, linestyle=':')
     grid(color='black', linestyle='--', linewidth=0.7, alpha=0.7)
     xlim([t_0, t_final])
     ylabel(r'Quaternion ${q}$')
@@ -276,9 +326,15 @@ def simulate_system(state_init, frequency, t_0, t_final):
 
 
     figure()
-    text = ['$x$', '$y$', '$z$']
+    text = ['$x_{real}$', '$y_{real}$', '$z_{real}$']
+    text1 = ['$x$', '$y$', '$z$']
+    text2 = ['$x_{des}$', '$y_{des}$', '$z_{des}$']
+    colors1 = ['orange', 'y', 'k']
+    styles = ['-.', '--', '-']
     for i in range(3):
-        plot(t, states[:,i], linewidth=2.0, label=str(text[i]))
+        plot(t, states[:, i], color=str(colors[i]), linewidth=2.0, linestyle=str(styles[i]), label=str(text[i]))
+        plot(t, states_mod[:, i], color=str(colors1[i]), linewidth=2.0, linestyle=str(styles[i]), label=str(text1[i]))
+        plot(t, poss_d[:, i], color=str(colors[i]), linewidth=1.5, linestyle=':', label=str(text2[i]))
     grid(color='black', linestyle='--', linewidth=0.7, alpha=0.7)
     xlim([t_0, t_final])
     ylabel(r'Position ${p}$')
@@ -325,6 +381,20 @@ def simulate_system(state_init, frequency, t_0, t_final):
     title('Attitude control')
     savefig('Brick/attitude_control.png')
 
+
+    figure()
+    text = ['$F_{1}}$', '$F_{2}}$', '$F_{3}}$', '$F_{4}}$', '$F_{5}}$', '$F_{6}}$', '$F_{7}}$', '$F_{8}}$']
+    for i in range(8):
+        plot(t, Fs[:, i], linewidth=2.0, label=str(text[i]))
+    grid(color='black', linestyle='--', linewidth=0.7, alpha=0.7)
+    xlim([t_0, t_final])
+    ylabel(r'Force ${F}$')
+    xlabel(r'Time $t$ (s)')
+    legend(loc='lower right')
+    title('Forces')
+    savefig('Brick/force.png')
+
+
     close('all')
     # show()
 
@@ -344,23 +414,10 @@ def simulate_system(state_init, frequency, t_0, t_final):
 
     def animate(num):
         ax.clear()
-        xc, yc, zc = data[0, num], data[1, num], data[2, num]
-        dx = length / 2
-        dy = width / 2
-        roll, pitch, yaw = get_current_orientation(num)
+        rc = np.array([data[0, num], data[1, num], data[2, num]])
         quat = get_current_quaternion(num)
-        Hc = Tx(xc) @ Ty(yc) @ Tz(zc) @ quaternion_affine_matrix(quat, [0, 0, 0]) #Rx(roll) @ Ry(pitch) @ Rz(yaw)
-        H = Hc @ Tx(dx) @ Ty(-dy)
-        p1 = H[:3, 3]
 
-        H = Hc @ Tx(-dx) @ Ty(-dy)
-        p2 = H[:3, 3]
-
-        H = Hc @ Tx(-dx) @ Ty(dy)
-        p3 = H[:3, 3]
-
-        H = Hc @ Tx(dx) @ Ty(dy)
-        p4 = H[:3, 3]
+        p1, p2, p3, p4 = get_points(rc, quat, length, width)
 
         points = np.vstack([p1, p2, p3, p4])
         # points = np.array([[-1, -1, -1],
@@ -382,9 +439,11 @@ def simulate_system(state_init, frequency, t_0, t_final):
                 ]
 
         ax.scatter3D(data[0, 0], data[1, 0], data[2, 0], color='b')
-        ax.scatter3D(data[0, -1], data[1, -1], data[2, -1], color='r')
 
-        ax.scatter3D(xc, yc, zc, color='b', alpha=.50)
+        state_d = trajectory_params['state_d']
+        ax.scatter3D(state_d[0], state_d[1], state_d[2], color='r')
+
+        ax.scatter3D(rc[0], rc[1], rc[2], color='b', alpha=.50)
         ax.scatter3D(Z[:, 0], Z[:, 1], Z[:, 2], color='b', alpha=.50)
         collection = Poly3DCollection(verts, facecolors='b', linewidths=1, edgecolors='r', alpha=.10)
         ax.add_collection3d(collection)
@@ -405,54 +464,62 @@ def simulate_system(state_init, frequency, t_0, t_final):
     ani = animation.FuncAnimation(fig, animate, frames=np.shape(data)[1],
                                     interval=dT) #repeat=False,
                                     
-    writer = animation.PillowWriter(fps=100)
-    ani.save('Brick/animation.gif', writer=writer)
+    # writer = animation.PillowWriter(fps=100)
+    # ani.save('Brick/animation.gif', writer=writer)
     show()
 
 
 t0 = 0
 tf = 10.0
-freq = 100
+freq = 100 # 500
 dT = 1/freq
-
-# Brick parameters
-length = 0.8
-width = 0.4
-height = 0.1
-m = 1.3
-Ixx = Iyy = 0.5  # 6.5 * 10**(-4)
-Izz = 0.4  # 1.2*10**(-3)
-Ixy = Iyx = 0.001
-Ixz = Izx = 0.001
-Iyz = Izy = 0.001
 g = 9.81
 
-I = np.array([[Ixx, Ixy, Ixz],
-              [Iyx, Iyy, Iyz],
-              [Izx, Izy, Izz]])
+# Brick parameters
+length = 0.5
+width = 0.5
+height = 0.2
 
-K1 = np.diag([15, 15, 15])
-K2 = np.diag([5, 5, 5])
+m_real = 12
 
-K_pos = np.block([K1, K1])
-K_att = np.block([K2, K2])
+mat = np.array([[(width/2) ** 2 + (height/2) ** 2, 0.001, 0.001],
+                    [0.001, (height/2) ** 2 + (length/2) ** 2, 0.001],
+                    [0.001, 0.001, (length/2) ** 2 + (width/2) ** 2]]) / 3
+
+I_real = m_real * mat
 
 system_params = {'length': length,
                  'width': width,
                  'height': height,
-                 'm': m,
-                 'I': I,
-                 'Ixx': Ixx,
-                 'Iyy': Iyy,
-                 'Izz': Izz,
-                 'Ixy': Ixy,
-                 'Ixz': Ixz,
-                 'Iyz': Iyz,
+                 'm': m_real,
+                 'I': I_real,
                  'g': g,
                  }
 
+
+m_modified = m_real * 0.8
+I_modified = m_modified * mat
+
+u_min = -60.0
+u_max = 60.0
+
+# gains for orientational components: Kp_w, Kd_w
+K1 = np.diag([15, 15, 15])
+K2 = np.diag([10, 10, 10])
+
+# gains for positional components: Kp_v, Kd_v
+K3 = np.diag([15, 15, 30])
+K4 = np.diag([25, 25, 25])
+
+K_pos = np.block([K1, K1])
+K_att = np.block([K2, K2])
+
 control_params = {'K1': K1, 'K2': K2,
+                  'K3': K3, 'K4': K4,
                   'K_pos': K_pos, 'K_att': K_att,
+                  'u_min': u_min, 'u_max': u_max,
+                  'm': m_modified,
+                  'I': I_modified,
                   }
 
 
@@ -467,21 +534,20 @@ control_params = {'K1': K1, 'K2': K2,
 # # Convert the rotation to Euler angles given the axes of rotation
 # print(rot.as_euler('xyz'))
 
-quat_0 = Rotation.from_euler('xyz', [0, 0, 0], degrees=True).as_quat()
-quat_d = Rotation.from_euler('xyz', [-20, 30, 30], degrees=True).as_quat()
+quat_0 = Rotation.from_euler('xyz', [10, 10, 5], degrees=True).as_quat()
+quat_d = Rotation.from_euler('xyz', [0, 0, 0], degrees=True).as_quat()
 print(quat_0, quat_d)
 
 
 # initial position, velocity, quaternion, angular velocity
-x0 = [1., 2.0, 0.0,
+x0 = [0., 1.0, 0.0,
       0, 0, 0,
-      0, 0, 0, 0,
+      quat_0[3], quat_0[0], quat_0[1], quat_0[2], 
       0., 0., 0.]
-x0[6:10] = quat_0
 
-trajectory_params = {'state_d': [0.5, 2.5, 1,
+trajectory_params = {'state_d': [0.3, 0.7, 0.5,
                                  0., 0., 0.,
-                                 quat_d[0], quat_d[1], quat_d[2], quat_d[3],
+                                 quat_d[3], quat_d[0], quat_d[1], quat_d[2], 
                                  0., 0., 0.]}
 
 simulate_system(state_init=x0, frequency=freq, t_0=t0, t_final=tf)
--- Brick/matrices.py
+++ Brick/matrices.py
@@ -48,19 +48,62 @@ def Tz(z):
     return T
 
 
+def cross_product(p, q):
+    return [p[1]*q[2] - p[2]*q[1],
+            p[2]*q[0] - p[0]*q[2],
+            p[0]*q[1] - p[1]*q[0]]
 
+
+def quaternion_product(p,q):
+    Q = np.array([[p[0], -p[1], -p[2], -p[3]],
+                  [p[1],  p[0], -p[3],  p[2]],
+                  [p[2],  p[3],  p[0], -p[1]],
+                  [p[3], -p[2],  p[1],  p[0]]])
+    return Q @ q
+
+
+def conjugate(q):
+    return np.array([q[0], -q[1], -q[2], -q[3]])
+
+
+def skew(v):
+    return np.array([[0, -v[2], v[1]],
+                     [v[2], 0, -v[0]],
+                     [-v[1], v[0], 0]])
+
+
+def vect2quat(v):
+    return np.concatenate([[0], v])
+
+
+def quat2vect(q):
+    return q[1:]
+
+
+def ln_quat(q):
+    mod_vectq = np.linalg.norm(q[1:])
+    if mod_vectq < 1e-15:
+        return np.zeros(3)
+    else:
+        return np.array(q[1:])*np.arccos(q[0])*mod_vectq**(-1)
+
+
+def quat2euler_angles(q):
+    phi = np.arctan2((2 * (q[0] * q[1] + q[2] * q[3])), (1 - 2 * (q[1] ** 2 + q[2] ** 2)))
+    theta = np.arcsin(2 * (q[0] * q[2] - q[3] * q[1]))
+    psi = np.arctan2((2 * (q[0] * q[3] + q[1] * q[2])), (1 - 2 * (q[2] ** 2 + q[3] ** 2)))
+    return np.array([phi, theta, psi])
+
+
+def euler2quat(roll, pitch, yaw):
+        qx = np.sin(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) - np.cos(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        qy = np.cos(roll/2) * np.sin(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.cos(pitch/2) * np.sin(yaw/2)
+        qz = np.cos(roll/2) * np.cos(pitch/2) * np.sin(yaw/2) - np.sin(roll/2) * np.sin(pitch/2) * np.cos(yaw/2)
+        qw = np.cos(roll/2) * np.cos(pitch/2) * np.cos(yaw/2) + np.sin(roll/2) * np.sin(pitch/2) * np.sin(yaw/2)
+        return [qw, qx, qy, qz]
+
+        
 def quaternion_affine_matrix(Q, pos):
-    """
-    Covert a quaternion into a full three-dimensional rotation matrix.
- 
-    Input
-    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
- 
-    Output
-    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
-             This rotation matrix converts a point in the local reference 
-             frame to a point in the global reference frame.
-    """
     # Extract the values from Q
     q0 = Q[0]
     q1 = Q[1]
@@ -89,3 +132,45 @@ def quaternion_affine_matrix(Q, pos):
                            [0, 0, 0, 1]])
                             
     return aff_matrix
+
+
+def quaternion_rotation_matrix(Q):
+    """
+    Covert a quaternion into a full three-dimensional rotation matrix.
+ 
+    Input
+    :param Q: A 4 element array representing the quaternion (q0,q1,q2,q3) 
+ 
+    Output
+    :return: A 3x3 element matrix representing the full 3D rotation matrix. 
+             This rotation matrix converts a point in the local reference 
+             frame to a point in the global reference frame.
+    """
+    # Extract the values from Q
+    q0 = Q[0]
+    q1 = Q[1]
+    q2 = Q[2]
+    q3 = Q[3]
+     
+    # First row of the rotation matrix
+    r00 = q0 ** 2 + q1 ** 2 - q2 ** 2 - q3 ** 2
+    r01 = 2 * (q1 * q2 - q0 * q3)
+    r02 = 2 * (q1 * q3 + q0 * q2)
+     
+    # Second row of the rotation matrix
+    r10 = 2 * (q1 * q2 + q0 * q3)
+    r11 = q0 ** 2 - q1 ** 2 + q2 ** 2 - q3 ** 2
+    r12 = 2 * (q2 * q3 - q0 * q1)
+     
+    # Third row of the rotation matrix
+    r20 = 2 * (q1 * q3 - q0 * q2)
+    r21 = 2 * (q2 * q3 + q0 * q1)
+    r22 = q0 ** 2 - q1 ** 2 - q2 ** 2 + q3 ** 3
+     
+    # 3x3 rotation matrix
+    rot_matrix = np.array([[r00, r01, r02],
+                           [r10, r11, r12],
+                           [r20, r21, r22],
+                           ])
+                            
+    return rot_matrix
